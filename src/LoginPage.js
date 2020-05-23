5283fdce7778bc737663012fdcd35f1c9948f716
Tekerey <WagglyComb216@gmail.com>
2020-05-23 22:19:15 +0300

    Done

--- src/LoginPage.js
+++ src/LoginPage.js
@@ -8,54 +8,142 @@ class LoginPage extends React.Component {
     constructor(props) {
         super(props);
         this.handleLogin = this.handleLogin.bind(this);
+        this.handleFocus = this.handleFocus.bind(this);
+        this.state = {
+            emailError: false,
+            emailErrorText: '',
+            passwordError: false,
+            passwordErrorText: ''
+        };
     }
 
-    handleLogin(e) {
-        e.preventDefault();
-        const formData = new FormData(document.getElementById('loginForm'));
-        const body = {
-            email: formData.get('email'),
-            password: formData.get('password')
-        };
-
-        fetch('https://localhost:44384/api/test/login', {
-            method: 'POST',
-            credentials: 'include',
-            headers: {
-                'Content-Type': 'application/json',
-            },
-            body: JSON.stringify(body),
-        })
-        .then(
-            response => {
-                // Добавить возможность указывать редирект ссылку
-                if (response.ok) this.props.history.push('/');
-            },
-            error => {
-                console.log(error);
+    componentDidMount() {
+        AuthService.auth().then(
+            (res) => {
+                if (res.isAuth) {
+                    this.props.history.push('/');
+                }
             }
         );
     }
 
-    render() {
-        // здесь вставить запрос на сервер с проверкой валидности токена
-        // вместо простой проверки наличия токена. Тогда не придётся удалять куки при редиректе на логин.
-        if (Cookies.getCookie('token')) {
-            return <Redirect to='/' />;
+    handleFocus(e) {
+        const el = e.target;
+        el.classList.remove('error');
+        if (el.id == 'email') {
+            this.setState({
+                emailError: false
+            });
+        } else if (el.id == 'password') {
+            this.setState({
+                passwordError: false
+            });
+        }
+    }
+
+    handleLogin(e) {
+        e.preventDefault();
+        document.getElementById('email').classList.remove('error');
+        document.getElementById('password').classList.remove('error');
+
+        const formData = new FormData(document.getElementById('loginForm'));
+        const body = {
+            email: formData.get('email'),
+            password: formData.get('password'),
+            rememberMe: formData.get('remember') == null ? false : true
+        };
+
+        //console.log(body.password); Как защититься от такого ???
+
+        const noEmail = body.email.trim().length == 0;
+        const noPassword = body.password.trim().length == 0;
+
+        let errors = false;
+
+        if (noEmail && noPassword) {
+            this.setState({
+                emailError: true,
+                emailErrorText: 'Enter email.',
+                passwordError: true,
+                passwordErrorText: 'Enter password'
+            });
+            errors = true;
+        } else if (noEmail) {
+            this.setState({
+                emailError: true,
+                emailErrorText: 'Enter email.',
+                passwordError: false
+            });
+            errors = true;
+        } else if (noPassword) {
+            this.setState({
+                emailError: false,
+                passwordError: true,
+                passwordErrorText: 'Enter password'
+            });
+            errors = true;
         }
         
+        if (!noPassword && body.password.trim().length <= 6) {
+            this.setState({
+                passwordError: true,
+                passwordErrorText: 'Password must have more than 6 symbols.'
+            });
+            errors = true;
+        }
+
+        if (!errors) {
+            AuthService.login(body)
+            .then(
+                result => {
+                    if (result.isLogged) {
+                        // Добавить возможность указывать редирект ссылку
+                        this.props.history.push('/');
+                    } else {
+                        if (result.status == 'email') {
+                            document.getElementById('email').classList.add('error');
+                            this.setState({
+                                emailError: true,
+                                emailErrorText: 'No user with this email.',
+                                passwordError: false});
+                        } else {
+                            document.getElementById('password').classList.add('error');
+                            this.setState({
+                                emailError: false,
+                                passwordError: true,
+                                passwordErrorText: 'Incorrect password.'
+                            });
+                        }
+                    }
+                },
+                error => {
+                    console.log(error);
+                }
+            );            
+        }
+    }
+
+    render() {
         return (
             <div className='LoginPage'>
                 <form id='loginForm' onSubmit={this.handleLogin}>
                     <h2>Log to Web App</h2><hr></hr>
                     <div className='FormBody'>
-                        <label htmlFor='email'>E-mail</label>
-                        <input type='text' name='email' id='email'></input>
-                        <label htmlFor='password'>Password</label>
-                        <input type='password' name='password' id='password'></input>
+                        <label className='inputLabel' htmlFor='email'>E-mail:</label>
+                        <input type='email' name='email' id='email' onFocus={this.handleFocus}></input>
+                        {this.state.emailError &&
+                            <div className='errorInfo'>
+                                {this.state.emailErrorText}
+                            </div>}
+                        <label className='inputLabel' htmlFor='password'>Password:</label>
+                        <input type='password' name='password' id='password' onFocus={this.handleFocus}></input>
+                        {this.state.passwordError &&
+                            <div className='errorInfo'>
+                                {this.state.passwordErrorText}
+                            </div>}
                         <span>
                             <input type='checkbox' name='remember' id='remember'></input>
-                            <label htmlFor='remember'>Remember me</label>
+                            <label htmlFor='remember'> Remember me</label>
                         </span>
                         <button type='submit'>Login</button>
                     </div>
--- src/helpers/authService.js
+++ src/helpers/authService.js
@@ -1,29 +1,43 @@
-import Cookies from './cookies';
 
 class AuthService {
-    static auth() {
-        return fetch('https://localhost:44384/api/test/auth', {
+    static async auth() {
+        const response = await fetch('https://localhost:44384/api/test/auth', {
             method: 'GET',
             credentials: 'include'
-        })
-        .then(
-            response => {
-                if (!response.ok) {
-                    Cookies.deleteCookie('token');
-                    Cookies.deleteCookie('email');
-                    Cookies.deleteCookie('login');
-                    return false;
-                    // this.props.history.push('/login');
-                } else {
-                    return true;
-                    // this.content = <WeatherBlock />;
-                    // this.setState({isLoaded: true});
-                }
+        });
+        if (response.status == 401) {
+            const data = await response.json();
+            //this.logout(); // Убрать и делать логаут на стороне сервера?
+            return {isAuth: false, reason: data.reason};
+        } else if (response.ok) {
+            return {isAuth: true};
+        } else {
+            return {isAuth: false, reason: 'error'};
+        }
+    }
+
+    static logout() {
+        return fetch('https://localhost:44384/api/test/logout', {
+            method: 'POST',
+            credentials: 'include'
+        });
+        // Отправка запроса на логаут на сервер? удаление куки на стороне сервера? удаление токена с бд?
+        // Куки с токеном должен быть HttpOnly, и удалятся/изменятся на стороне сервера?
+    }
+
+    static async login(body) {
+        const response = await fetch('https://localhost:44384/api/test/login', {
+            method: 'POST',
+            credentials: 'include',
+            headers: {
+                'Content-Type': 'application/json',
             },
-            error => {
-                console.log(error);
-            }
-        );
+            body: JSON.stringify(body),
+        });
+
+        if (response.ok) return {isLogged: true};
+        const data = await response.json();
+        return {isLogged: false, status: data.status};
     }
 }
 
